e39db4c5a21dda725c80e9c5e139cc4fd65da201
Kevin C Malin <racmalin@yahoo.com>
2020-06-11 15:34:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,14 +50,14 @@ class Song
 end 
 
 lucifer = Song.new("Luicifer", "Jay-Z", "rap")
-tomorow = Song.new("Tomorow", "John Smith", "classic")
+ = Song.new("Tomorow", "John Smith", "classic")
 a_yearz = Song.new("A Yearz", "Adam Johns", "oldtime")
 a_rabess = Song.new("Arabess", "Habibi Watan", "Rai")
 cus_umak = Song.new("Cus Ummach", "Haifa Wehbe", "Rai")
 
-puts the_day.name
-puts the_day.artist
-puts the_day.genre
+puts lucifer.name
+puts lucifer.artist
+puts lucifer.genre
 puts Song.count
 Song.genres
 Song.artists
