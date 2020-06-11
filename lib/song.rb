ed9c62e1d01cd6d47857228bf0a461e22d9d3961
Kevin C Malin <racmalin@yahoo.com>
2020-06-11 15:38:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ end
 
 lucifer = Song.new("Luicifer", "Jay-Z", "rap")
 ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
-hit_me = Song.new("hit me baby one more time", "Brittney", "oldtime")
+hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop")
 
 puts lucifer.name
 puts lucifer.artist
